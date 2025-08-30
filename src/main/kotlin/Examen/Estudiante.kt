3228699f850e591b1cd0b603706ee0ac61dc1cc3
rafaelchuco <rafael.chuco@tecsup.edu.pe>
2025-08-30 18:23:15 -0500

    feat: add exercise 2 (Student class with primary constructor) in Poo_Examen_kotlin.kt

---
+++ src/main/kotlin/Examen/Estudiante.kt
@@ -0,0 +1,8 @@
+package Examen
+
+class Estudiante(val codigo: String, val nombre: String)
+fun main() {
+    val estudiante = Estudiante("112345", "Rafael")
+    println("Bienvenido")
+    println("Estimado alumno ${estudiante.nombre} su codigo es ${estudiante.codigo}")
+}
